21cd1ffa710e9af95709d0e2c9c864fee3a03365
gewoehnlich <oomyse@gmail.com>
2025-06-01 18:42:32 +0500

    learning testing

---
+++ tests/Feature/Tasks/UpdateTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature\Tasks;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class Update extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
