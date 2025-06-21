c68ea9326604b6b2b23e6538ee7f371439525744
gewoehnlich <oomyse@gmail.com>
2025-06-21 15:02:08 +0500

    added PHPDoc properties, so intelephense wouldn't be mad

--- app/DTO/API/Tasks/TaskDTO.php
+++ app/DTO/API/Tasks/TaskDTO.php
@@ -5,6 +5,22 @@ namespace App\DTO\API\Tasks;
 use App\DTO\API\APIDTO;
 use App\Http\Requests\API\Tasks\TaskRequest;
 
+/**
+ * @property-read int    $id
+ * @property-read int    $userId
+ * @property-read string $title
+ * @property-read string $description
+ * @property-read Stage  $Stage
+ * @property-read Carbon $deadline
+ * @property-read Carbon $createdAtFrom
+ * @property-read Carbon $createdAtTo
+ * @property-read Carbon $updatedAtFrom
+ * @property-read Carbon $updatedAtTo
+ * @property-read Carbon $deadlineFrom
+ * @property-read Carbon $deadlineTo
+ * @property-read string $orderBy
+ * @property-read int    $limit
+ */
 abstract class TaskDTO extends APIDTO
 {
     final public static function fromRequest(
