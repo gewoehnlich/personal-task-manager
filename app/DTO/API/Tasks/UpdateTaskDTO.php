9e908ca2627b02ac4f48148499183dcba0049d0c
gewoehnlich <oomyse@gmail.com>
2025-06-22 16:00:00 +0500

    addew final keyword to properties and new properties

--- app/DTO/API/Tasks/UpdateTaskDTO.php
+++ app/DTO/API/Tasks/UpdateTaskDTO.php
@@ -8,12 +8,14 @@ use Illuminate\Support\Carbon;
 
 final class UpdateTaskDTO extends TaskDTO
 {
-    public readonly int $id;
-    public readonly int $userId;
-    public readonly string $title;
-    public readonly string $description;
-    public readonly Stage $stage;
-    public readonly Carbon $deadline;
+    final public readonly int $id;
+    final public readonly int $userId;
+    final public readonly string $title;
+    final public readonly string $description;
+    final public readonly Stage $stage;
+    final public readonly Carbon $deadline;
+    final public readonly int | null $parentId;
+    final public readonly int | null $projectId;
 
     public function __construct(UpdateTaskRequest $request)
     {
@@ -21,7 +23,9 @@ final class UpdateTaskDTO extends TaskDTO
         $this->userId      = $request->user_id;
         $this->title       = $request->title;
         $this->description = $request->description;
-        $this->stage       = $request->stage;
-        $this->deadline    = $request->deadline;
+        $this->stage       = self::stage($request->stage);
+        $this->deadline    = self::date($request->deadline);
+        $this->parentId = $request->parent_id;
+        $this->projectId = $request->project_id;
     }
 }
