62411f380d103c31027904a4feb5375282be3bb8
gewoehnlich <oomyse@gmail.com>
2025-06-22 15:56:44 +0500

    added final keyword to properties

--- app/DTO/API/Tasks/DeleteTaskDTO.php
+++ app/DTO/API/Tasks/DeleteTaskDTO.php
@@ -6,8 +6,8 @@ use App\Http\Requests\API\Tasks\DeleteTaskRequest;
 
 final class DeleteTaskDTO extends TaskDTO
 {
-    public readonly int $id;
-    public readonly int $userId;
+    final public readonly int $id;
+    final public readonly int $userId;
 
     public function __construct(DeleteTaskRequest $request)
     {
