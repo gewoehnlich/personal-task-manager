e6961855a779f976b2fe17e8a5a89ca6e8a2ce2e
gewoehnlich <oomyse@gmail.com>
2025-10-02 23:38:51 +0500

    first method 'create user token' is ready

--- app/Containers/Auth/Actions/CreateUserTokenAction.php
+++ app/Containers/Auth/Actions/CreateUserTokenAction.php
@@ -3,22 +3,41 @@
 namespace App\Containers\Auth\Actions;
 
 use App\Containers\Auth\Tasks\CheckIfUserTokenAlreadyExistsTask;
+use App\Containers\Auth\Tasks\GenerateUserTokenTask;
 use App\Containers\Auth\Transporters\CreateUserTokenTransporter;
 use App\Ship\Abstracts\Responders\Responder;
 use App\Ship\Parents\Actions\Action;
+use App\Ship\Parents\Exceptions\Exception;
 
 final readonly class CreateUserTokenAction extends Action
 {
     public function run(
         CreateUserTokenTransporter $transporter,
     ): Responder {
-        $tokenExists = $this->task(
-            CheckIfUserTokenAlreadyExistsTask::class,
-            user: $transporter->user,
-        );
+        try {
+            $tokenExists = $this->task(
+                CheckIfUserTokenAlreadyExistsTask::class,
+                user: $transporter->user,
+            );
 
-        return $this->success([
-            'result' => $tokenExists,
-        ]);
+            if ($tokenExists) {
+                return $this->error(
+                    message: 'your token already exists! run update or delete',
+                );
+            }
+
+            $token = $this->task(
+                GenerateUserTokenTask::class,
+                user: $transporter->user,
+            );
+
+            return $this->success(
+                data: ['result' => $token]
+            );
+        } catch (Exception $exception) {
+            return $this->error(
+                message: $exception->getErrors(),
+            );
+        }
     }
 }
