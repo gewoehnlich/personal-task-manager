75574ce0ca97c6cbfc6296baf09b3187f038b192
gewoehnlich <oomyse@gmail.com>
2025-10-04 15:46:22 +0500

    experimenting with criteria

--- app/Containers/Tasks/Actions/IndexTasksAction.php
+++ app/Containers/Tasks/Actions/IndexTasksAction.php
@@ -2,6 +2,7 @@
 
 namespace App\Containers\Tasks\Actions;
 
+use App\Containers\Tasks\Criteria\FilterByUserIdCriterion;
 use App\Containers\Tasks\Transporters\IndexTasksTransporter;
 use App\Containers\Tasks\Models\Task;
 use App\Containers\Tasks\Repositories\TaskRepository;
@@ -21,62 +22,70 @@ final readonly class IndexTasksAction extends ActionsAction
         IndexTasksTransporter $transporter,
     ): Responder {
         try {
-            $query = Task::query();
+            $this->repository->pushCriteria(
+                criteria: new FilterByUserIdCriterion(
+                    userId: $transporter->userId,
+                ),
+            );
 
-            if (isset($transporter->userId)) {
-                $query->where('user_id', $transporter->userId);
-            }
+            $result = $this->repository->get();
 
-            if (isset($transporter->id)) {
-                $query->where('id', $transporter->id);
-            }
-
-            if (isset($transporter->stage)) {
-                $query->where('stage', $transporter->stage);
-            }
-
-            if (isset($transporter->parentId)) {
-                $query->where('parent_id', $transporter->parentId);
-            }
-
-            if (isset($transporter->projectId)) {
-                $query->where('project_id', $transporter->projectId);
-            }
-
-            if (isset($transporter->createdAtFrom)) {
-                $query->where('created_at', '>=', $transporter->createdAtFrom);
-            }
-
-            if (isset($transporter->createdAtTo)) {
-                $query->where('created_at', '<=', $transporter->createdAtTo);
-            }
-
-            if (isset($transporter->updatedAtFrom)) {
-                $query->where('updated_at', '>=', $transporter->updatedAtFrom);
-            }
-
-            if (isset($transporter->updatedAtTo)) {
-                $query->where('updated_at', '<=', $transporter->updatedAtTo);
-            }
-
-            if (isset($transporter->deadlineFrom)) {
-                $query->where('deadline', '>=', $transporter->deadlineFrom);
-            }
-
-            if (isset($transporter->deadlineTo)) {
-                $query->where('deadline', '<=', $transporter->deadlineTo);
-            }
-
-            if (isset($transporter->orderBy)) {
-                $orderByField = $transporter->orderByField ?? 'id';
-                $query->orderBy($orderByField, $transporter->orderBy);
-            }
-
-            if (isset($transporter->limit)) {
-                $query->limit($transporter->limit);
-            }
-
-            $result = $query->get();
+            // $query = Task::query();
+            //
+            // if (isset($transporter->userId)) {
+            //     $query->where('user_id', $transporter->userId);
+            // }
+            //
+            // if (isset($transporter->id)) {
+            //     $query->where('id', $transporter->id);
+            // }
+            //
+            // if (isset($transporter->stage)) {
+            //     $query->where('stage', $transporter->stage);
+            // }
+            //
+            // if (isset($transporter->parentId)) {
+            //     $query->where('parent_id', $transporter->parentId);
+            // }
+            //
+            // if (isset($transporter->projectId)) {
+            //     $query->where('project_id', $transporter->projectId);
+            // }
+            //
+            // if (isset($transporter->createdAtFrom)) {
+            //     $query->where('created_at', '>=', $transporter->createdAtFrom);
+            // }
+            //
+            // if (isset($transporter->createdAtTo)) {
+            //     $query->where('created_at', '<=', $transporter->createdAtTo);
+            // }
+            //
+            // if (isset($transporter->updatedAtFrom)) {
+            //     $query->where('updated_at', '>=', $transporter->updatedAtFrom);
+            // }
+            //
+            // if (isset($transporter->updatedAtTo)) {
+            //     $query->where('updated_at', '<=', $transporter->updatedAtTo);
+            // }
+            //
+            // if (isset($transporter->deadlineFrom)) {
+            //     $query->where('deadline', '>=', $transporter->deadlineFrom);
+            // }
+            //
+            // if (isset($transporter->deadlineTo)) {
+            //     $query->where('deadline', '<=', $transporter->deadlineTo);
+            // }
+            //
+            // if (isset($transporter->orderBy)) {
+            //     $orderByField = $transporter->orderByField ?? 'id';
+            //     $query->orderBy($orderByField, $transporter->orderBy);
+            // }
+            //
+            // if (isset($transporter->limit)) {
+            //     $query->limit($transporter->limit);
+            // }
+            //
+            // $result = $query->get();
 
             return $this->success(
                 data: ['result' => $result],
---
+++ app/Containers/Tasks/Criteria/FilterByUserIdCriterion.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Containers\Tasks\Criteria;
+
+use App\Ship\Parents\Criteria\Criterion;
+use Prettus\Repository\Contracts\RepositoryInterface;
+
+final readonly class FilterByUserIdCriterion extends Criterion
+{
+    public function __construct(
+        public int $userId,
+    ) {
+        //
+    }
+
+    public function apply(
+        $model,
+        RepositoryInterface $repository,
+    ) {
+        return $model->where('user_id', $this->userId);
+    }
+}
