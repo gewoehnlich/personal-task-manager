c4aeb5e6cc4435a0547f9cd4fce73bec793f82c2
gewoehnlich <oomyse@gmail.com>
2025-10-04 12:49:15 +0500

    working index tasks functionality

--- app/Containers/Tasks/Actions/IndexTasksAction.php
+++ app/Containers/Tasks/Actions/IndexTasksAction.php
@@ -6,69 +6,78 @@ use App\Containers\Tasks\Transporters\IndexTasksTransporter;
 use App\Containers\Tasks\Models\Task;
 use App\Ship\Abstracts\Responders\Responder;
 use App\Ship\Parents\Actions\Action as ActionsAction;
+use App\Ship\Parents\Exceptions\Exception;
 
 final readonly class IndexTasksAction extends ActionsAction
 {
     public function run(
         IndexTasksTransporter $transporter,
     ): Responder {
-        $query = Task::query();
+        try {
+            $query = Task::query();
 
-        if (!empty($transporter->userId)) {
-            $query->where('user_id', $transporter->userId);
+            if (isset($transporter->userId)) {
+                $query->where('user_id', $transporter->userId);
+            }
+
+            if (isset($transporter->id)) {
+                $query->where('id', $transporter->id);
+            }
+
+            if (isset($transporter->stage)) {
+                $query->where('stage', $transporter->stage);
+            }
+
+            if (isset($transporter->parentId)) {
+                $query->where('parent_id', $transporter->parentId);
+            }
+
+            if (isset($transporter->projectId)) {
+                $query->where('project_id', $transporter->projectId);
+            }
+
+            if (isset($transporter->createdAtFrom)) {
+                $query->where('created_at', '>=', $transporter->createdAtFrom);
+            }
+
+            if (isset($transporter->createdAtTo)) {
+                $query->where('created_at', '<=', $transporter->createdAtTo);
+            }
+
+            if (isset($transporter->updatedAtFrom)) {
+                $query->where('updated_at', '>=', $transporter->updatedAtFrom);
+            }
+
+            if (isset($transporter->updatedAtTo)) {
+                $query->where('updated_at', '<=', $transporter->updatedAtTo);
+            }
+
+            if (isset($transporter->deadlineFrom)) {
+                $query->where('deadline', '>=', $transporter->deadlineFrom);
+            }
+
+            if (isset($transporter->deadlineTo)) {
+                $query->where('deadline', '<=', $transporter->deadlineTo);
+            }
+
+            if (isset($transporter->orderBy)) {
+                $orderByField = $transporter->orderByField ?? 'id';
+                $query->orderBy($orderByField, $transporter->orderBy);
+            }
+
+            if (isset($transporter->limit)) {
+                $query->limit($transporter->limit);
+            }
+
+            $result = $query->get();
+
+            return $this->success(
+                data: ['result' => $result],
+            );
+        } catch (Exception $exception) {
+            return $this->error(
+                message: $exception->getErrors(),
+            );
         }
-
-        if (!empty($transporter->id)) {
-            $query->where('id', $transporter->id);
-        }
-
-        if (!empty($transporter->stage)) {
-            $query->where('stage', $transporter->stage);
-        }
-
-        if (!empty($transporter->parentId)) {
-            $query->where('parent_id', $transporter->parentId);
-        }
-
-        if (!empty($transporter->projectId)) {
-            $query->where('project_id', $transporter->projectId);
-        }
-
-        if (!empty($transporter->createdAtFrom)) {
-            $query->where('created_at', '>=', $transporter->createdAtFrom);
-        }
-
-        if (!empty($transporter->createdAtTo)) {
-            $query->where('created_at', '<=', $transporter->createdAtTo);
-        }
-
-        if (!empty($transporter->updatedAtFrom)) {
-            $query->where('updated_at', '>=', $transporter->updatedAtFrom);
-        }
-
-        if (!empty($transporter->updatedAtTo)) {
-            $query->where('updated_at', '<=', $transporter->updatedAtTo);
-        }
-
-        if (!empty($transporter->deadlineFrom)) {
-            $query->where('deadline', '>=', $transporter->deadlineFrom);
-        }
-
-        if (!empty($transporter->deadlineTo)) {
-            $query->where('deadline', '<=', $transporter->deadlineTo);
-        }
-
-        if (!empty($transporter->orderBy)) {
-            $orderByField = $transporter->orderByField ?? 'id';
-            $query->orderBy($orderByField, $transporter->orderBy);
-        }
-
-        if (!empty($transporter->limit)) {
-            $query->limit($transporter->limit);
-        }
-
-        $result = $query->get();
-
-        return new TaskResource($result);
     }
 }
