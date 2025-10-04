2910033797d4ac14432104c30ab1bf55c9513713
gewoehnlich <oomyse@gmail.com>
2025-10-04 14:17:16 +0500

    base Criterion classes

---
+++ app/Ship/Abstracts/Criteria/Criterion.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Ship\Abstracts\Criteria;
+
+use Prettus\Repository\Contracts\CriteriaInterface;
+
+abstract readonly class Criterion implements CriteriaInterface
+{
+    //
+}
---
+++ app/Ship/Parents/Criteria/Criterion.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Ship\Parents\Criteria;
+
+use App\Ship\Abstracts\Criteria\Criterion as AbstractCriterion;
+
+abstract readonly class Criterion extends AbstractCriterion
+{
+    //
+}
