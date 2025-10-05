5d78d5ec060bf54ed3f42bc32a8e02c3f6bf2ec5
gewoehnlich <oomyse@gmail.com>
2025-10-05 23:50:22 +0500

    bills table update

--- database/migrations/2025_06_05_121424_create_bills_table.php
+++ database/migrations/2025_06_05_121424_create_bills_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
                 ->references('id')
                 ->on('tasks')
                 ->onDelete('cascade');
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->unsignedBigInteger('time_spent');
-            $table->timestamp('performed_at');
+            $table->timestamp('performed_at')->useCurrent();
             $table->timestamps();
         });
     }
