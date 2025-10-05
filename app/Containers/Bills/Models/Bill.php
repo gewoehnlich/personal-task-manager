1e19d4c673bccaf8a0dda747719ea5a412122ff1
gewoehnlich <oomyse@gmail.com>
2025-10-05 23:51:49 +0500

    bill model update

--- app/Containers/Bills/Models/Bill.php
+++ app/Containers/Bills/Models/Bill.php
@@ -2,29 +2,11 @@
 
 namespace App\Containers\Bills\Models;
 
+use App\Containers\Tasks\Models\Task;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-/**
- * @property int $id
- * @property int $task_id
- * @property string $description
- * @property int $time_spent
- * @property string $performed_at
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Bill newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Bill newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Bill query()
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Bill whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Bill whereDescription($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Bill whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Bill wherePerformedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Bill whereTaskId($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Bill whereTimeSpent($value)
- * @method static \Illuminate\Database\Eloquent\Builder<static>|Bill whereUpdatedAt($value)
- * @mixin \Eloquent
- */
 final class Bill extends Model
 {
     use HasFactory;
@@ -35,4 +17,20 @@ final class Bill extends Model
         'time_spent',
         'performed_at',
     ];
+
+    protected $hidden = [
+        //
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public function task(): BelongsTo
+    {
+        return $this->belongsTo(Task::class);
+    }
 }
