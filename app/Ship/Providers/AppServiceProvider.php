465687ddcdfa1251975aec9271d61b7cc2e30307
gewoehnlich <oomyse@gmail.com>
2025-10-19 18:59:31 +0500

    PTM-6 feat: trying to enable https on the server

--- app/Ship/Providers/AppServiceProvider.php
+++ app/Ship/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Ship\Providers;
 
 use App\Ship\Actions\MigrationsContainersGetAction;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
             $this->loadMigrationsFrom(
                 $folder,
             );
+        }
+
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
         }
     }
 }
